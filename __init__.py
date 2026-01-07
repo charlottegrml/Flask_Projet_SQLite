138bc47b6b5429d0cc537ebce8454dfacdad23f7
charlottegrml <charlotte.gourmelond@gmail.com>
2026-01-07 15:36:47 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,6 +16,10 @@ def est_authentifie():
 def hello_world():
     return render_template('hello.html')
 
+@app.route('/fiche_nom', methods=['GET'])
+def formulaire_client():
+    return render_template('') 
+
 @app.route('/lecture')
 def lecture():
     if not est_authentifie():
